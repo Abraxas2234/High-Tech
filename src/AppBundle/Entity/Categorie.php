02aaffd4d9d147e16821a37b68bded8794c6ddbc
Abraxas <jerome.griebel@epitech.eu>
2018-07-09 14:36:27 +0200

    Auto stash before merge of "master" and "origin/fos-user-front"

--- src/AppBundle/Entity/Categorie.php
+++ src/AppBundle/Entity/Categorie.php
@@ -19,14 +19,48 @@ class Categorie
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    public $id;
+    private $id;
 
     /**
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=100, nullable=false)
      */
-    public $name;
+    private $name;
 
 
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Categorie
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
